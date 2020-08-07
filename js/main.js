ed4c39278afe8d5426e75acf6d8f127d3dee43ca
Laraib <laraib24@outlook.com>
2020-08-07 20:41:36 +0500

    started adding js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 
-
+function myFunction() {
+    var x = document.getElementById("lefSide");
+    var y = document.getElementById("inScreen");
+    var z = document.getElementById("threadOne");
+    document.getElementById("inScreen").style.visibility = "hidden";
+    document.getElementById("threadOne").style.visibility = "visible";
+  
+  };
