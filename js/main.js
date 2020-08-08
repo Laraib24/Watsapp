f0c2504d1ad09796f1107c0e39e939650a918613
Laraib <laraib24@outlook.com>
2020-08-08 14:32:42 +0500

    first onclick event

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 
 function myFunction() {
-    var x = document.getElementById("lefSide");
-    var y = document.getElementById("inScreen");
-    var z = document.getElementById("threadOne");
-    document.getElementById("inScreen").style.visibility = "hidden";
-    document.getElementById("threadOne").style.visibility = "visible";
-  
+  document.getElementById("lefSide").style.visibility = "hidden";
+  document.getElementById("inScreen").style.visibility = "hidden";
+  document.getElementById("threadOne").style.visibility = "visible";
   };
