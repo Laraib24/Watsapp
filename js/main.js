2b6aee9d0adc055728d8e0dc64a6612a75ab9f11
Laraib <laraib24@outlook.com>
2020-08-08 16:46:21 +0500

    onclick event Js added

--- js/main.js
+++ js/main.js
@@ -3,9 +3,54 @@ function firstThread() {
   document.getElementById("lefSide").style.visibility = "hidden";
   document.getElementById("inScreen").style.visibility = "hidden";
   document.getElementById("threadOne").style.visibility = "visible";
-  document.getElementById("threadtwo").style.visibility = "hidden";
+  document.getElementById("threadTwo").style.visibility = "hidden";
+  document.getElementById("threadThree").style.visibility = "hidden";
+  document.getElementById("threadFour").style.visibility = "hidden";
+  document.getElementById("threadFifth").style.visibility = "hidden";
+  document.getElementById("threadSix").style.visibility = "hidden";
   };
-  function secondThread() {
+function secondThread() {
+    document.getElementById("inScreen").style.visibility = "hidden";
     document.getElementById("threadOne").style.visibility = "hidden";
-    document.getElementById("threadtwo").style.visibility = "visible";
+    document.getElementById("threadTwo").style.visibility = "visible";
+    document.getElementById("threadThree").style.visibility = "hidden";
+    document.getElementById("threadFour").style.visibility = "hidden";
+    document.getElementById("threadFifth").style.visibility = "hidden";
+    document.getElementById("threadSix").style.visibility = "hidden";
     };
+function thirdThread() {
+      document.getElementById("inScreen").style.visibility = "hidden";
+      document.getElementById("threadOne").style.visibility = "hidden";
+      document.getElementById("threadTwo").style.visibility = "hidden";
+      document.getElementById("threadThree").style.visibility = "visible";
+      document.getElementById("threadFour").style.visibility = "hidden";
+      document.getElementById("threadFifth").style.visibility = "hidden";
+      document.getElementById("threadSix").style.visibility = "hidden";
+      };
+function fourThread() {
+        document.getElementById("inScreen").style.visibility = "hidden";
+        document.getElementById("threadOne").style.visibility = "hidden";
+        document.getElementById("threadTwo").style.visibility = "hidden";
+        document.getElementById("threadThree").style.visibility = "hidden";
+        document.getElementById("threadFour").style.visibility = "visible";
+        document.getElementById("threadFifth").style.visibility = "hidden";
+        document.getElementById("threadSix").style.visibility = "hidden";
+        };
+function fifthThread() {
+          document.getElementById("inScreen").style.visibility = "hidden";
+          document.getElementById("threadOne").style.visibility = "hidden";
+          document.getElementById("threadTwo").style.visibility = "hidden";
+          document.getElementById("threadThree").style.visibility = "hidden";
+          document.getElementById("threadFour").style.visibility = "hidden";
+          document.getElementById("threadFifth").style.visibility = "visible";
+          document.getElementById("threadSix").style.visibility = "hidden";
+          };
+function sixThread() {
+            document.getElementById("inScreen").style.visibility = "hidden";
+            document.getElementById("threadOne").style.visibility = "hidden";
+            document.getElementById("threadTwo").style.visibility = "hidden";
+            document.getElementById("threadThree").style.visibility = "hidden";
+            document.getElementById("threadFour").style.visibility = "hidden";
+            document.getElementById("threadFifth").style.visibility = "hidden";
+            document.getElementById("threadSix").style.visibility = "visible";
+            };
