6fb37b4e5f32fbfa726d81e64ab5d78e382972b1
Laraib <laraib24@outlook.com>
2020-08-08 16:00:36 +0500

    Second click event added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
-function myFunction() {
+function firstThread() {
   document.getElementById("lefSide").style.visibility = "hidden";
   document.getElementById("inScreen").style.visibility = "hidden";
   document.getElementById("threadOne").style.visibility = "visible";
+  document.getElementById("threadtwo").style.visibility = "hidden";
   };
+  function secondThread() {
+    document.getElementById("threadOne").style.visibility = "hidden";
+    document.getElementById("threadtwo").style.visibility = "visible";
+    };
